2e4aad271b6d82efc9692986f880183131d2a775
yoonbum.no <yoonbum.no@hp.com>
2019-04-01 17:41:31 +0900

    Add RentalInfo.

--- Refactoring/src/yb/main.java
+++ Refactoring/src/yb/main.java
@@ -5,6 +5,7 @@ public class main {
 		Customer customer1 = new Customer("YB");
 		customer1.addRental(new Rental(new Movie("titanic",Movie.NEW_RELEASE),5));
 		customer1.addRental(new Rental(new Movie("Bang",Movie.REGULAR),3));
+		customer1.addRental(new Rental(new Movie("BBoBBoRo",Movie.CHILDRENS),10));
 		
 		System.out.println(customer1.statement());
 		
